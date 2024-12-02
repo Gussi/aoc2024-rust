5d0400413f39533096d27f32d91829e1e26a08d1
Gunnsteinn Þórisson <gussi@gussi.is>
2024-12-02 00:50:15 +0000

    Day 1 cleanup

--- src/main.rs
+++ src/main.rs
@@ -38,14 +38,7 @@ fn main() {
 
 pub mod day1 {
     pub fn part1(input: &str) -> i32 {
-        let mut first_list_of_numbers = Vec::new();
-        let mut second_list_of_numbers = Vec::new();
-
-        for line in input.lines() {
-            let mut numbers = line.split("   ");
-            first_list_of_numbers.push(numbers.next().unwrap().parse::<i32>().unwrap());
-            second_list_of_numbers.push(numbers.next().unwrap().parse::<i32>().unwrap());
-        }
+        let (mut first_list_of_numbers, mut second_list_of_numbers) = get_list_of_numbers(input);
 
         first_list_of_numbers.sort();
         second_list_of_numbers.sort();
@@ -65,26 +58,11 @@ pub mod day1 {
     }
 
     pub fn part2(input: &str) -> i32 {
-        let mut first_list_of_numbers = Vec::new();
-        let mut second_list_of_numbers = Vec::new();
-
-        for line in input.lines() {
-            let mut numbers = line.split("   ");
-            first_list_of_numbers.push(numbers.next().unwrap().parse::<i32>().unwrap());
-            second_list_of_numbers.push(numbers.next().unwrap().parse::<i32>().unwrap());
-        }
-
-        first_list_of_numbers.sort();
-        second_list_of_numbers.sort();
+        let (first_list_of_numbers, second_list_of_numbers) = get_list_of_numbers(input);
 
         let mut total_similarity = 0;
-
-        // Loop through first list of numbers
         for number in first_list_of_numbers.iter() {
-            // Find number of occurences of this number in second list
             let occurences = second_list_of_numbers.iter().filter(|&n| n == number).count() as i32;
-
-            // Multiply our number by the number of occurences, add to total similarity
             total_similarity += number * occurences;
         }
 
@@ -95,5 +73,18 @@ pub mod day1 {
     fn test_part2() {
         let input = "3   4\n4   3\n2   5\n1   3\n3   9\n3   3";
         assert_eq!(part2(input), 31);
+    }
+
+    fn get_list_of_numbers(input: &str) -> (Vec<i32>, Vec<i32>) {
+        let mut first_list_of_numbers = Vec::new();
+        let mut second_list_of_numbers = Vec::new();
+
+        for line in input.lines() {
+            let mut numbers = line.split("   ");
+            first_list_of_numbers.push(numbers.next().unwrap().parse::<i32>().unwrap());
+            second_list_of_numbers.push(numbers.next().unwrap().parse::<i32>().unwrap());
+        }
+
+        (first_list_of_numbers, second_list_of_numbers)
     }
 }
